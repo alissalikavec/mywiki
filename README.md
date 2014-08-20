66178a9fdbe1920907f52a6bdf4425188e46079a
Alex Spencer <alex.spencer@adfitech.com>
2014-08-20 17:59:40 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# MyWiki
+
+A humble wikipedia clone
+
+## Contributing
+
+1. Fork Repo
+2. Clone Fork
+3. Setup your postgres connection
+4. 
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mywiki::Application.config.secret_token = 'b2e5a7230db4675fd4f7ac33567a430940c0a98f0eb1b6274bed7d451ad85f1d80c81d4712b33a035942f6acb680f26e38f309b5e101356dc5d6539f33404803'
